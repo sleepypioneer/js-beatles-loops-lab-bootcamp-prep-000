75d26436a2f3f3709bef24acb76dee971a10f3ec
Jessica Greene <jessica0greene@gmail.com>
2017-09-20 16:09:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-function theBeatlesPlay( musicians, instruments){
+function theBeatlesPlay(musicians, instruments){
   var array = []
-  for (i = 0; i < instruments.length+1; i++ ){
-    array.push(musicians[i] + plays + instruments[i])
+  for (i=0; i < musicians.length+1; i++){
+    array.push(musician[i] + "plays" + instruments[i])
   }
+  return array
+}
+
+function johnLennonFacts(facts){
+  var array = [];
+  var i=0;
+  while(i < facts.length+1){
+    array.push(facts[i] + "!!!")
+    i++
+  }
+  return array
+}
+
+function iLoveTheBeatles(number){
+  var array = []
+  do {
+    array.push("I love the Beatles!")
+    number++
+  } while ( number < 15)
+
+  return array
 }
