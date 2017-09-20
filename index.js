7e61b9a674e7dccdba87d14dea82869cfa0960b4
Jessica Greene <jessica0greene@gmail.com>
2017-09-20 16:15:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function theBeatlesPlay(musicians, instruments){
   var array = []
+  var i = 0
   for (i=0; i < musicians.length+1; i++){
     array.push(musician[i] + "plays" + instruments[i])
+    i++
   }
   return array
 }
